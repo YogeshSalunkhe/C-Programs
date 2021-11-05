50966b8807e22d5c450c91d28b33e896a3e8d779
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2021-11-05 22:11:14 +0530

    Update Cheking Given Character Is UPPER CASE or Lower Case or DIGIT or Special Symbols.c

--- Cheking Given Character Is UPPER CASE or Lower Case or DIGIT or Special Symbols.c
+++ Cheking Given Character Is UPPER CASE or Lower Case or DIGIT or Special Symbols.c
@@ -9,13 +9,13 @@ int main()
   char ch = '\0';
   
   printf("\n \n Enter A Character =");
-  ch = getchar ();
+  ch = getche ();
   
   if( ch >= 'A' && ch <= 'Z')
   {
     printf("\n\n %c is upper case alphabet.",ch);
   }
-  else if(ch >=  'a' && ch <=  'z')
+  else if(ch >= 'a' && ch <= 'z')
   {
     printf("\n %c is lower case alphabet.",ch);
   }

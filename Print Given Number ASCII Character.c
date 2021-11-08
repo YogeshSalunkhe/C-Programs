8603ec67f5abf1a3af8bae894e02a274b17b1c8a
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2021-11-08 20:39:35 +0530

    Add files via upload

---
+++ Print Given Number ASCII Character.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+ int No = 0;
+  printf("\n\t Print ASCII Character Of Given Number =>");
+  scanf("%d",&No);
+  
+  printf("\n\t Character Of Given %d is %c",No,No);
+  
+  getch();
+  return 0;
+}

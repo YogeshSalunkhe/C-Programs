8cad6d93148aae1df83a39da9a1bb7191026eeb5
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2021-12-02 16:46:38 +0530

    Add files via upload

---
+++ Array Programs/Garbage Value In Array.c
@@ -0,0 +1,19 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Values[5] = {45,23};
+  
+  printf("\n=================++*****++=================\n");
+   
+  printf("\n Values Of 1st Element = %d",Values[0]);
+  printf("\n Values Of 2nd Element = %d",Values[1]);
+  printf("\n Values Of 3rd Element = %d",Values[2]);
+  printf("\n Values Of 4th Element = %d",Values[3] );
+  printf("\n Values Of 5th Element = %d",Values[4]);
+  
+  printf("\n\n=================++*****++=================\n");
+  return 0;
+}

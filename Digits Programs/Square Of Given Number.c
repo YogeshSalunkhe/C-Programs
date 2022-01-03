743c6c9c53ea980e87492773270e4427cbd3b499
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-01-03 21:54:37 +0530

    Add files via upload

---
+++ Digits Programs/Square Of Given Number.c
@@ -0,0 +1,16 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No = 0;
+  
+  printf("\n\tEnter A Number To Get Its Square => ");
+  scanf("%d",&No);
+  
+  printf("\n\t Square Of Given Number %d = %d ",No,(No*No));
+  
+  getch();
+  return 0;
+}

7a58823f815594281fa97e0aa8f6653c78d5023e
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-02-05 22:42:03 +0530

    Add files via upload

---
+++ Pattern Printing Programs/Pattern Of PUNE.c
@@ -0,0 +1,88 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Display_Pune(int N)
+{
+  int i,j;
+  
+  for( i = 1; i <= N; i++)
+  {
+    for(j = 1; j <= N;j++)
+    {
+      if(i == 1 && j != N||j == 1||i == (N+1)/2 && j != N || j== N && i<= (N+1)/2)
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  
+  printf("\n\n");
+  for( i = 1; i <= N; i++)
+  {
+    for(j = 1; j <= N;j++)
+    {
+      if(j == 1 && i != N|| j == N && i != N|| i == N && j != 1 && j != N)
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  
+   printf("\n\n");
+  for( i = 1; i <= N; i++)
+  {
+    for(j = 1; j <= N;j++)
+    {
+      if(j == 1 || j == N || i == j)
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+ 
+  printf("\n\n");
+  for( i = 1; i <= N; i++)
+  {
+    for(j = 1; j <= N;j++)
+    {
+      if(j == 1 || i == 1 || i == N || i == (N+1)/2)
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  
+}
+  
+
+
+int main()
+{
+  int x;
+  printf("\n\t Enter Size => ");
+  scanf("%d",&x);
+  
+  Display_Pune(x);
+  
+  return 0;
+}

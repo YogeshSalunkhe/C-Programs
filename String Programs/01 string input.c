96061e7e858dd81516524147cdc485f1f5c0310c
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-04-02 22:20:51 +0530

    Add files via upload

---
+++ String Programs/01 string input.c
@@ -0,0 +1,16 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+int main()
+{
+  char Name[10]="";
+  
+  
+  printf("\n\t Enter Name => ");
+  gets(Name);
+  
+  puts("\n\t Name is ");
+  puts(Name);
+  
+  return 0;
+}

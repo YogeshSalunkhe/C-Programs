d5ff46c71b95c255257ec589000018a33ce57e95
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-04-14 22:08:36 +0530

    Add files via upload

---
+++ String Programs/07 string copy.c
@@ -0,0 +1,24 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+int main()
+{
+  char Src[50] = "";
+  int I = 0,j = 0;
+  
+  printf("\n\t Enter Name => ");
+  gets(Src);
+  
+  for(I = 0;Src[I] != '\0';I++);
+
+  
+  for(j = 0,I = 0; Src[I] != '\0' ;I--,j++)
+  {
+     Src[j] = Src[I];
+    
+  }
+  
+  printf("\n\t Aft4 Of  string = %s",Src);
+  
+  return 0;
+}

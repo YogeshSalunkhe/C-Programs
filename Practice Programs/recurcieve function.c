85c7d6c72a5b0f1d1cb4630ddd9ed2cab125e8ea
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-05-18 22:04:18 +0530

    Add files via upload

---
+++ Practice Programs/recurcieve function.c
@@ -0,0 +1,29 @@
+//Compiler version gcc  6.3.0
+
+#include <stdio.h>
+#include <conio.h>
+
+int fact1(int No)
+{
+ if(No == 0 || No == 1)
+ {
+   return 1;
+ }
+ 
+ return No * fact1(No -1);
+}
+
+
+int main()
+{
+  int Num = 0,Fact;
+ 
+  printf("\n\t Enter No => ");
+  scanf("%d",&Num);
+ 
+ Fact=fact1(Num);
+ 
+ printf("\n\t Factorial Of Given Number %d = %d",Num,Fact);
+  
+  return 0;
+} 
